170c0eb5d88984bb11777c78f137dd05466215d7
kriti <kriti.jindal.37@gmail.com>
2023-11-08 10:03:37 +0530

    added multiple cards

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-const request = new XMLHttpRequest();
-request.open('GET', 'https://dummyjson.com/users/21');
-request.send();
-const containerEle = document.querySelector('#container');
-request.addEventListener("load", () => {
-    console.log((request.responseText));
-    let data = JSON.parse(request.responseText);
-    console.log(data);
 
-    const doc = `<div>
+function displayCards(id) {
+    const request = new XMLHttpRequest();
+    request.open('GET', `https://dummyjson.com/users/${id}`);
+    request.send();
+    const containerEle = document.querySelector('#container');
+    request.addEventListener("load", () => {
+        console.log((request.responseText));
+        let data = JSON.parse(request.responseText);
+        console.log(data);
+
+        const doc = `<div>
 <image src=${data.image} alt="not found"></image>
 <h3>${data.firstName}</h3>
 <h3>${data.lastName}</h3>
 <h3>${data.email}</h3>
 </div>`
-    containerEle.innerHTML = doc;
-});
+        containerEle.insertAdjacentHTML("beforeend", doc) = doc;
+    });
+
+}
+
+for (let i = 1; i < 10; i++) {
+    displayCards(i);
+}
+
+// displayCards(10);
+// displayCards(1);
 
 
 
